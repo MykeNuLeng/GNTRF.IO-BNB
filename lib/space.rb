90501c14d307ae7c243bc028f309f7399eaf3a93
Alex Withington-Smith <alex@awithsmith.com>
2020-11-23 17:35:35 +0000

    list of available properties passing first test

---
+++ lib/space.rb
@@ -0,0 +1,7 @@
+class Space
+  def self.all_spaces
+    # returning empty list, which will remain the base case
+    # if there are no listings
+    []
+  end
+end
--- spec/features/browse_listings_spec.rb
+++ spec/features/browse_listings_spec.rb
@@ -1,11 +1,5 @@
 require 'capybara/rspec'
-
-# testing only, remove!!!
-class Listing
-  def self.all_spaces
-
-  end
-end
+require 'space'
 
 feature "browse listings" do
   scenario "there are no listings" do
@@ -14,24 +8,11 @@ feature "browse listings" do
   end
 
   scenario "displays the headline and price of a list of one Listing" do
-    listing = double :listing, headline: "Phwoar, what a listing", price: 6000
-    class_double = double('Listing')
-    allow(Listing).to receive(:all_spaces).and_return([listing])
+    space = double :space, headline: "Phwoar, what a listing", price: 6000
+    class_double = double('Space')
+    visit '/listings'
+    allow(Space).to receive(:all_spaces).and_return([space])
     expect(page).to have_content("Phwoar, what a listing")
     expect(page).to have_content("£60")
   end
 end
-
-
-# feature "navigation bar" do
-#   scenario "has all of the required elements" do
-#     visit('/')
-#     expect(page).to have_button('Sign Up')
-#     expect(page).to have_button('Sign In')
-#   end
-
-#   scenario "has log off button if logged in" do
-#     visit('/logged-in-test')
-#     expect(page).to have_button('Log Off')
-#   end
-# end
