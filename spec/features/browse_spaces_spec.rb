1ef8e7302f85479ad44b0ee3cebd7d4578bbe7dc
Alex Withington-Smith <alex@awithsmith.com>
2020-11-25 11:17:58 +0000

    made sure that logo links to home, home links to listings if logged in

--- spec/features/browse_spaces_spec.rb
+++ spec/features/browse_spaces_spec.rb
@@ -1,10 +1,10 @@
 require 'capybara/rspec'
 require 'space'
 
-feature "browse listings" do
-  scenario "there are no listings" do
+feature "browse spaces" do
+  scenario "there are no spaces" do
     visit '/spaces'
-    expect(page).to have_content "NO CURRENT LISTINGS!"
+    expect(page).to have_content "NO CURRENT SPACES!"
   end
 
   scenario "displays the headline and price of a list of one space" do
@@ -24,5 +24,16 @@ feature "browse listings" do
     expect(page).to have_content("£54")
     expect(page).to have_content("Lovely property")
     expect(page).to have_content("£69")
+  end
+
+  scenario "logo redirects logged in user to spaces" do
+    User.create(email: "test@test.com", username: "brian", password: "test123")
+    visit '/'
+    fill_in('email', with: 'test@test.com')
+    fill_in('password', with: 'test123')
+    click_button('LOGIN')
+    find('.logo').click
+    expect(current_path).to eq '/spaces'
+    save_and_open_page('./test_output.html')
   end
 end
