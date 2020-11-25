5b3ba99bca5cde15e7ad808ebda565cb21f18d30
Michael Linford <linford.mike@gmail.com>
2020-11-25 12:07:20 +0000

    wrote a test for new spaces

---
+++ spec/features/new_space_spec.rb
@@ -0,0 +1,25 @@
+feature 'new space' do
+  before do
+    User.create(email: "test@test.com", username: "brian", password: "test123")
+    visit '/'
+    fill_in('email', with: 'test@test.com')
+    fill_in('password', with: 'test123')
+    click_button('LOGIN')
+    click_button 'NEW LISTING'
+  end
+
+  scenario 'clicking on new listing takes you to the page' do
+    expect(current_path).to eq '/spaces/new'
+  end
+
+  scenario 'being on spaces/new means you can fill in a form' do
+    expect(page).to have_css('.logo')
+    expect(page).not_to have_link('Log In')
+    expect(page).to have_button 'PROFILE'
+    expect(page).to have_button 'LOG OUT'
+    expect(page).to have_button 'NEW LISTING'
+    expect(page).to have_field('headline')
+    expect(page).to have_field('description')
+    expect(page).to have_field('picture')
+  end
+end
