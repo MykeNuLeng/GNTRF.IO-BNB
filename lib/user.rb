ba44487cd8a55c45b2b186c48cfe43159c115737
Alex Withington-Smith <alex@awithsmith.com>
2020-11-26 06:08:34 +0000

    email/username now downcased by default to avoid case sensitivity, passing all tests

--- lib/user.rb
+++ lib/user.rb
@@ -14,9 +14,9 @@ class User
   def self.create(username:, password:, email:)
     encrypted_password = BCrypt::Password.create(password)
     result = DatabaseConnection.query("INSERT INTO users (username, password, email)
-                              VALUES ('#{username}', '#{encrypted_password}', '#{email}')
-                              RETURNING id;")
-    User.new(user_id: result[0]["id"], username: username, email: email)
+                              VALUES ('#{username.downcase}', '#{encrypted_password}', '#{email.downcase}')
+                              RETURNING id, username, email;")
+    User.new(user_id: result[0]["id"], username: result[0]["username"], email: result[0]["email"])
   end
 
   def self.find(user_id:)
@@ -27,7 +27,7 @@ class User
 
   def self.authenticate(email:, password:)
     user_info = DatabaseConnection.query("SELECT * FROM users
-                                 WHERE email = '#{email}';")
+                                 WHERE email = '#{email.downcase}';")
     return false unless user_info.cmd_tuples > 0 # nb that cmd_tuples is a pg object attribute for the number of database lines returned
     return false unless BCrypt::Password.new(user_info[0]["password"]) == password # nb might cause a problem if email's not unique
     User.new(user_id: user_info[0]["id"], username: user_info[0]["username"], email: email)
