694a4bcad0765aa225a8d659f1a88671ec9c5762
Michael Linford <linford.mike@gmail.com>
2020-11-26 17:30:39 +0000

    can reject and accept bookings

--- app.rb
+++ app.rb
@@ -84,4 +84,14 @@ class Controller < Sinatra::Base
     @lettings = Order.order_history_by_landlord_id(user_id: session[:user].user_id)
     erb :'profile/lettings'
   end
+
+  get '/profile/lettings/:id/reject' do
+    Order.reject(order_id: params[:id])
+    redirect '/profile/lettings'
+  end
+
+  get '/profile/lettings/:id/confirm' do
+    Order.confirm(order_id: params[:id])
+    redirect '/profile/lettings'
+  end
 end
--- spec/features/view_lettings_spec.rb
+++ spec/features/view_lettings_spec.rb
@@ -73,39 +73,39 @@ feature "List of lettings" do
     expect(page).to have_button("CONFIRM")
     expect(page).to have_button("REJECT")
     click_button 'CONFIRM'
-    expect(page).to have_content 'confirmed'
+    expect(page).to have_content 'CONFIRMED'
   end
 
-  # scenario "removes the booking when you reject" do
-  #   tenant = User.create(email: "test@test.com", username: "brian", password: "Testlength123")
-  #   landlord = User.create(email: "leech@test.com", username: "parasite", password: "Testlength123")
-  #
-  #   space = Space.create(
-  #     user_id: landlord.user_id,
-  #     price: 6900,
-  #     headline: "Amazing space",
-  #     description: "test"
-  #   )
-  #   Order.create(
-  #     space_id: space.space_id,
-  #     user_id: tenant.user_id,
-  #     booking_start: "2021-04-19",
-  #     booking_end: "2021-04-21"
-  #   )
-  #   visit('/')
-  #   fill_in('email', with: 'leech@test.com')
-  #   fill_in('password', with: 'Testlength123')
-  #   click_button('LOGIN')
-  #   click_button('PROFILE')
-  #   click_link('Lettings')
-  #   expect(page).to have_content(space.headline)
-  #   expect(page).to have_content("19/04/21 - 21/04/21")
-  #   expect(page).to have_button("CONFIRM")
-  #   expect(page).to have_button("REJECT")
-  #   click_button 'REJECT'
-  #   expect(page).not_to have_content(space.headline)
-  #   expect(page).not_to have_content("19/04/21 - 21/04/21")
-  #   expect(page).not_to have_button("CONFIRM")
-  #   expect(page).not_to have_button("REJECT")
-  # end
+  scenario "removes the booking when you reject" do
+    tenant = User.create(email: "test@test.com", username: "brian", password: "Testlength123")
+    landlord = User.create(email: "leech@test.com", username: "parasite", password: "Testlength123")
+
+    space = Space.create(
+      user_id: landlord.user_id,
+      price: 6900,
+      headline: "Amazing space",
+      description: "test"
+    )
+    Order.create(
+      space_id: space.space_id,
+      user_id: tenant.user_id,
+      booking_start: "2021-04-19",
+      booking_end: "2021-04-21"
+    )
+    visit('/')
+    fill_in('email', with: 'leech@test.com')
+    fill_in('password', with: 'Testlength123')
+    click_button('LOGIN')
+    click_button('PROFILE')
+    click_link('Lettings')
+    expect(page).to have_content(space.headline)
+    expect(page).to have_content("19/04/21 - 21/04/21")
+    expect(page).to have_button("CONFIRM")
+    expect(page).to have_button("REJECT")
+    click_button 'REJECT'
+    expect(page).not_to have_content(space.headline)
+    expect(page).not_to have_content("19/04/21 - 21/04/21")
+    expect(page).not_to have_button("CONFIRM")
+    expect(page).not_to have_button("REJECT")
+  end
 end
