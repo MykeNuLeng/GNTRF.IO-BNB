7a0cc2727478cd6e4984512ce5ba0cc0f867b883
Alex Withington-Smith <alex@awithsmith.com>
2020-11-26 05:57:06 +0000

    removed unneccessary notification for test database - we will be notified if setup unsuccessful by failing tests

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,10 +1,7 @@
 require 'pg'
 
 def setup_test_database
-  p "Setting up test database..."
-
   connection = PG.connect(dbname: 'bnb_test')
-
   # This stops the cascade warning message
   connection.exec("set client_min_messages='warning';")
   # Clear test table
