8fca4c9d176ac03088622dd502a86583217aa2f6
Alex Withington-Smith <alex@awithsmith.com>
2020-11-26 16:50:15 +0000

    added test for lettings page content

--- spec/features/view_lettings_spec.rb
+++ spec/features/view_lettings_spec.rb
@@ -16,4 +16,32 @@ feature "List of lettings" do
     expect(page).to have_content 'MY LETTINGS'
     expect(page).to have_content 'No lettings to show yet!'
   end
+
+  scenario "it displays lettings" do
+    tenant = User.create(email: "test@test.com", username: "brian", password: "Testlength123")
+    landlord = User.create(email: "leech@test.com", username: "parasite", password: "Testlength123")
+
+    space = Space.create(
+      user_id: landlord.user_id,
+      price: 6900,
+      headline: "Amazing space",
+      description: "test"
+    )
+    Order.create(
+      space_id: space.space_id,
+      user_id: tenant.user_id,
+      booking_start: "2021-04-19",
+      booking_end: "2021-04-21"
+    )
+    visit('/')
+    fill_in('email', with: 'leech@test.com')
+    fill_in('password', with: 'Testlength123')
+    click_button('LOGIN')
+    click_button('PROFILE')
+    click_link('Lettings')
+    expect(page).to have_content(space.headline)
+    expect(page).to have_content("19/04/21 - 21/04/21")
+    expect(page).to have_button("CONFIRM")
+    expect(page).to have_button("REJECT")
+  end
 end
