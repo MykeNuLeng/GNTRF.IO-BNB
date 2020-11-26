130aff482ca78fa405664d36a2ba8b7a674ec75c
Alex Withington-Smith <alex@awithsmith.com>
2020-11-26 16:24:18 +0000

    added first test for base case of lettings:

---
+++ spec/features/view_lettings_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../feature_spec_helper.'
+require 'capybara/rspec'
+
+feature "List of lettings" do
+  scenario "it displays a list of empty lettings if there are none" do
+    create_account_and_login
+    visit('/profile/lettings')
+    expect(page).to have_content('No lettings to show yet!')
+  end
+end
