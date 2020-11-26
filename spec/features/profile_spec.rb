39f2457f247f93a508de47026ef93b6593bb9fbb
Alex Withington-Smith <alex@awithsmith.com>
2020-11-26 10:06:09 +0000

    added basic structure for profile

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -20,4 +20,17 @@ feature "Profile page" do
     click_button('PROFILE')
     expect(current_path).to eq('/profile')
   end
+
+  scenario "has all the correct elements when logged in" do
+    create_account_and_login
+    visit('/spaces')
+    expect(page).to have_css '.logo'
+    expect(page).to have_button 'PROFILE'
+    expect(page).to have_button 'LOG OFF'
+    expect(page).to have_button 'NEW LISTING'
+    expect(page).not_to have_link 'Bookings'
+    expect(page).to have_link 'Spaces'
+    expect(page).to have_content 'MY BOOKINGS'
+    expect(page).to have_content 'No bookings to show yet!'
+  end
 end
