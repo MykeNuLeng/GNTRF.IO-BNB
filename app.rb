d67c64fe5fd7aacd86269beb73c7fe3811cb2a8e
Alex Withington-Smith <alex@awithsmith.com>
2020-11-27 07:04:53 +0000

    undid my mess, tests all passing. back to a branch where I belong now

--- app.rb
+++ app.rb
@@ -68,14 +68,12 @@ class Controller < Sinatra::Base
   end
 
   post '/spaces/new' do
-    image = params[:image].empty? ? nil : params[:image]
-
     Space.create(
       user_id: session[:user].user_id,
       price: params[:price].to_i * 100,
       headline: params[:headline],
       description: params[:description],
-      image: image
+      image: params[:photo]
     )
 
     redirect('/spaces')
