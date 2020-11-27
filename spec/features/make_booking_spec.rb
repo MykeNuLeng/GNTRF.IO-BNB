cf11e21c7f103e85db408c539126949b9f4233e4
Alex Withington-Smith <alex@awithsmith.com>
2020-11-27 12:47:55 +0000

    added test for button that clicks to create a booking

---
+++ spec/features/make_booking_spec.rb
@@ -0,0 +1,16 @@
+require 'capybara/rspec'
+require 'space'
+require 'user'
+require_relative '../feature_spec_helper'
+
+feature "Make booking" do
+  scenario "accessible via the spaces page" do
+    landlord = User.create(username: "testy", password: "123Password", email: "testymctesterson@test.org")
+    space = Space.create(user_id: landlord.user_id, price: 5400, headline: "Kinda alright space", description: "Really cheap, really damp!")
+    user = create_account_and_login
+    visit('/spaces')
+    expect(page).to have_button('BOOK')
+    click_button('BOOK')
+    expect(current_path).to eq("/spaces/#{space.space_id}/book")
+  end
+end
