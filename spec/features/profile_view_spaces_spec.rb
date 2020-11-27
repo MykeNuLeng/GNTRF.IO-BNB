ba42332886d448e8e8c9b498a02eef3a09e91751
Alex Withington-Smith <alex@awithsmith.com>
2020-11-27 07:33:47 +0000

    added initial tests for spaces path and linking

---
+++ spec/features/profile_view_spaces_spec.rb
@@ -0,0 +1,26 @@
+require 'capybara/rspec'
+require 'order'
+require_relative '../feature_spec_helper'
+
+feature "Lettings page" do
+  scenario "the route exists" do
+    create_account_and_login
+    visit('/profile/spaces')
+    expect(page.status_code).to eq(200)
+  end
+
+  scenario "accessible by clicking link in profile (when logged in)" do
+    create_account_and_login
+    visit('/profile')
+    click_link('Spaces')
+    expect(current_path).to eq('/profile/spaces')
+  end
+
+  scenario "accessible by clicking button in lettings (when logged in)" do
+    create_account_and_login
+    visit('/profile/spaces')
+    click_button('PROFILE')
+    click_link('Spaces')
+    expect(current_path).to eq('/profile/spaces')
+  end
+end
