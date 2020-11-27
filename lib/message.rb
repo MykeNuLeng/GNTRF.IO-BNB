d2be7aa57b0fa4548082262826e75bb7d504ef51
Adam G <adamwizzy@hotmail.co.uk>
2020-11-27 10:55:14 +0000

    completed inbox & outbox

--- lib/message.rb
+++ lib/message.rb
@@ -16,8 +16,29 @@ class Message
     DatabaseConnection.query("INSERT INTO message (user_id_from, user_id_to, content, date, time) VALUES (#{my_id}, #{recipient_id}, '#{content}', '#{date}', '#{time}');")
   end
 
+  def self.get_inbox(user_id:)
+    result = DatabaseConnection.query("SELECT * FROM message WHERE user_id_to = #{user_id};")
+    result.map do |each|
+      Message.new(message_id: each["id"], user_id_from: each["user_id_from"], user_id_to: each["user_id_from"], content: each["content"], time: Message.time_from_database(each["date"], each["time"]))
+    end.sort_by(&:time).reverse
+  end
+
+  def self.get_outbox(user_id:)
+    result = DatabaseConnection.query("SELECT * FROM message WHERE user_id_from = #{user_id};")
+    result.map do |each|
+      Message.new(message_id: each["id"], user_id_from: each["user_id_from"], user_id_to: each["user_id_from"], content: each["content"], time: Message.time_from_database(each["date"], each["time"]))
+    end.sort_by(&:time).reverse
+  end
+
   private
   def self.sub_apostrophe(text)
     text.gsub "'", "&#39;"
+  end
+
+  def self.time_from_database(string_date, string_time)
+    date_array = string_date.split("-")
+    time_array = string_time.split(":")
+    Time.new(date_array[0], date_array[1], date_array[2],
+             time_array[0], time_array[1], time_array[2])
   end
 end
--- spec/unit/message_spec.rb
+++ spec/unit/message_spec.rb
@@ -4,12 +4,41 @@ require './lib/database_connection'
 
 describe Message do
 
-  it "Sends a message to the correct recipient when .send_message called" do
+  before do
     # need test users for valid user_id
-    test_user1 = User.create(username: "testy1", password: "123Password", email: "testymctesterson1@test.org")
-    test_user2 = User.create(username: "testy2", password: "123Password", email: "testymctesterson2@test.org")
-    # test
-    expect { Message.send_message(my_id: test_user1.user_id, recipient_id: test_user2.user_id, content: "Urgent please reply")}.to change { DatabaseConnection.query("SELECT * FROM message WHERE user_id_from = #{test_user1.user_id} AND user_id_to = #{test_user2.user_id};").cmd_tuples }.by(1)
+    @test_user1 = User.create(username: "testy1", password: "123Password", email: "testymctesterson1@test.org")
+    @test_user2 = User.create(username: "testy2", password: "123Password", email: "testymctesterson2@test.org")
+  end
+
+  it "Sends a message to the correct recipient when .send_message called" do
+    expect { Message.send_message(my_id: @test_user1.user_id, recipient_id: @test_user2.user_id, content: "Urgent please reply")}.to change { DatabaseConnection.query("SELECT * FROM message WHERE user_id_from = #{@test_user1.user_id} AND user_id_to = #{@test_user2.user_id};").cmd_tuples }.by(1)
+  end
+
+  context " Inbox & outbox-- " do
+
+    before do
+      Message.send_message(my_id: @test_user1.user_id, recipient_id: @test_user2.user_id, content: "chronological message 1")
+      sleep(2)
+      Message.send_message(my_id: @test_user2.user_id, recipient_id: @test_user1.user_id, content: "chronological message 2")
+      sleep(2)
+      Message.send_message(my_id: @test_user1.user_id, recipient_id: @test_user2.user_id, content: "chronological message 3")
+      sleep(2)
+      Message.send_message(my_id: @test_user2.user_id, recipient_id: @test_user1.user_id, content: "chronological message 4")
+    end
+
+    it "Builds correct inbox" do
+      inbox = Message.get_inbox(user_id: @test_user1.user_id)
+      expect(inbox).to be_instance_of(Array)
+      expect(inbox.length).to eq(2)
+      expect(inbox.first.content).to eq("chronological message 4")
+    end
+
+    it "Builds correct outbox" do
+      outbox = Message.get_outbox(user_id: @test_user1.user_id)
+      expect(outbox).to be_instance_of(Array)
+      expect(outbox.length).to eq(2)
+      expect(outbox.first.content).to eq("chronological message 3")
+    end
   end
 
 end
